e694e60f4fab0fccddee626c9c6002b8bfad0cec
Yasir Kadhim <95036084+YasirOdayGit@users.noreply.github.com>
2023-04-10 19:43:33 +0300

    chore: exported website statically

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+    reactStrictMode: true,
+    swcMinify: true,
+    output: 'export',
+    distDir: 'builtWebsite',
+    images: {
+        unoptimized: true,
+    },
 };
 
 module.exports = nextConfig;
